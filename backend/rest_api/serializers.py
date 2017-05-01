d9bf1d36f7d25a6ecb7c7546ce873791b206630a
swpp <greeksharifa@gmail.com>
2017-05-01 22:26:31 +0900

    login/signup

---
+++ backend/rest_api/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from core.models import BaseUser, Friend, Feed, Reply, Picture
+from django.db import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    #baseuser_nickname =
+    class Meta:
+        model = User
+        fields = ('id', 'username')#,'nickname')
