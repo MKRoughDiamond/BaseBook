c5b92b03e6b05d4f0a03849b3504aef393343921
greeksharifa <greeksharifa@gmail.com>
2017-05-08 19:16:00 +0900

    test code(updating)

--- backend/runtest.py
+++ backend/runtest.py
@@ -46,20 +46,31 @@ for i in range(1, N+1):
     print('3-{0}. user test{0} will post {1} feed'.format(i,M))
     NF += [M]
     F += M
-    auth_user = TL.auth_user_id_pwd(i)
+    uname = "test{0}".format(i)
+    upwd = "test{0}passwd".format(i)
     for j in range(1, M+1):
         print('posting feed{0}: '.format(j), end=' ')
         contents = 'contents of POST {0}-{1} feed: 종강하고싶다{1}{1}'.format(i,j)
         scope = scopes[(i%2)*2] #global or private, will be changed by i%4 later
-        TL.feed_test(link, contents, scope, auth_user)
+        TL.feed_post_test(link, contents, scope, uname, upwd)
 
 #####################################################
-print('4. GET feed/<id>')
+print('4. GET feed')
+link = localhost + 'feed/'
 
 for i in range(1, N+1):
-    print('4-{0}. user test{0} will get feed'.format(i))
+    uname = "test{0}".format(i)
+    upwd = "test{0}passwd".format(i)
+    TL.feed_get_test(link, uname, upwd)
+
+#####################################################
+print('5. GET feed/<id>')
+
+for i in range(1, N+1):
+    print('5-{0}. user test{0} will get feed'.format(i))
     sum = 0
-    auth_user = TL.auth_user_id_pwd(i)
+    uname = "test{0}".format(i)
+    upwd = "test{0}passwd".format(i)
     # see own's all post and (other's) one global post, one private post
     limit = NF[i]
     if i != N:
@@ -71,11 +82,11 @@ for i in range(1, N+1):
         seen = True
         if i != N and (sum+j) == limit-1:
             seen = False # other user's private feed
-        TL.feed_id_test(link, auth_user, seen)
+        TL.feed_id_get_test(link, uname, upwd, seen)
     pass
 
 #####################################################
-print('5. POST feed/<id>/reply')
+print('6. POST feed/<id>/reply')
 
 # number of reply of each feed
 NR = [0]
@@ -83,25 +94,32 @@ NR = [0]
 R = 0
 
 for i in range(1, N+1):
-    print('5-{0}. user test{0} will post {1} reply')
-    auth_user = TL.auth_user_id_pwd(i)
+    print('6-{0}. user test{0} will post {1} reply')
+    uname = "test{0}".format(i)
+    upwd = "test{0}passwd".format(i)
     for j in range(1, NF[i]+1):
         R += 1
         print('posting reply(my feed){0}: '.format(j), end=' ')
         link = localhost + 'feed/' + str(j) + '/reply/'
         contents = 'contents of POST {0}-{1} reply: 아무것도 안하고 싶다'.format(i,j)
-        TL.feed_reply_test(link, contents, auth_user)
+        TL.feed_reply_test(link, contents, uname, upwd)
     for j in range(1, N+1):
         if i==j: continue
         R += 1
         print('posting reply(other user\'s feed){0}: '.format(j), end=' ')
         link = localhost + 'feed/' + str(j) + '/reply/'
         contents = 'contents of POST {0}-{1} reply: 아무것도 안하고 싶다{1}{1}'.format(i, j)
-        TL.feed_reply_test(link, contents, auth_user)
+        TL.feed_reply_post_test(link, contents, uname, upwd)
     pass
 
 #####################################################
-print('6. profile')
+print('7. GET feed/<id>/reply')
+
+for i in range(1, N+1):
+    pass
+
+#####################################################
+print('8. profile')
 
 for i in range(1, N+1):
     #link = localhost + 'profile'
--- backend/testlibrary.py
+++ backend/testlibrary.py
@@ -1,6 +1,6 @@
 import requests, json
 from time import sleep
-import sys
+import sys, base64
 
 # (number of user)
 N = 5
@@ -26,7 +26,7 @@ def wrong_status_code(message, url, code):
 def unexpected_exception(message, url, e):
     print('\nException: cannot {0}'.format(message))
     print('url={0}'.format(url))
-    print(e)
+    print('Message: ', e)
     sys.exit(1)
 
 def signup_post_test(url, unickname, uname, upwd):
@@ -57,28 +57,60 @@ def login_post_test(url, uname, upwd):
         unexpected_exception('post login', url, e)
     print('success')
 
-def feed_test(url, contents, scope, auth_user):
+def feed_post_test(url, contents, scope, uname, upwd):
     sleep(0.05)
     try:
+        #hash = new Buffer(`${uname}:${upwd}`).toString('base64')
+        hash64 = base64.encodestring(uname + ":" + upwd)
         data = {'contents': contents, 'scope': scope}
-        headers = {'Content-Type': 'application/json'}
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + hash64
+        }
         res = requests.post(url,
                             data=json.dumps(data),
-                            headers=headers,
-                            auth=auth_user)
+                            headers=headers)
         if res.status_code != 200:
             wrong_status_code('post feed', url, res.status_code)
+
+        '''client = requests.Session()
+        csrftoken = client.get(url).cookies['csrftoken']
+        data = {'id':auth_user['id'], 'password':auth_user['password'],
+                    'contents':contents, 'scope':scope,
+                    'csrfmiddlewaretoken':csrftoken, 'next':'/'}
+        res = client.post(url,
+                          data=json.dumps(data),
+                          #headers=headers,
+                          auth=auth_user)
+        if res.status_code != 200:
+            wrong_status_code('post feed', url, res.status_code)'''
+        '''content = r.json()
+        status = content.get('status')
+        print('status code: {0}'.format(status), end=' ')
+        if status == 200:
+            print('good')'''
     except Exception as e:
         unexpected_exception('post feed', url, e)
     print('success')
 
-def feed_id_test(url, auth_user, seen):
+def feed_get_test(url, uname, upwd):
+    sleep(0.05)
+    try:
+        headers = {'Content-Type': 'application/json'}
+        res = requests.post(url,
+                            headers=headers)
+        if res.status_code != 200:
+            wrong_status_code('post feed', url, res.status_code)
+        pass
+    except Exception as e:
+        unexpected_exception('get feed', url, e)
+
+def feed_id_get_test(url, uname, upwd, seen):
     sleep(0.05)
     try:
         headers = {'Content-Type': 'application/json'}
         res = requests.get(url,
-                           headers = headers,
-                           auth=auth_user)
+                           headers = headers)
         if (seen and res.status_code!=200) or \
             (not seen and res.status_code==200):
             wrong_status_code('get feed', url, res.status_code)
@@ -86,11 +118,24 @@ def feed_id_test(url, auth_user, seen):
         unexpected_exception('get feed', url, e)
     print('success')
 
-def feed_reply_test(url, ):
+def feed_reply_post_test(url, contents, uname, upwd):
     sleep(0.05)
-    pass
+    try:
+        pass
+    except Exception as e:
+        unexpected_exception('get feed', url, e)
+
+def feed_reply_get_test(url, contents, uname, upwd):
+    sleep(0.05)
+    try:
+        pass
+    except Exception as e:
+        unexpected_exception('get feed', url, e)
 
 def profile_test(url, ):
     sleep(0.05)
-    pass
+    try:
+        pass
+    except Exception as e:
+        unexpected_exception('get feed', url, e)
 
