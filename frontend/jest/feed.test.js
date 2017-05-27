db78101ae6a1f0af4182798bdc8c7c0fe007352c
hyoputer <ajtwlsalsdl0@naver.com>
2017-05-27 13:42:21 +0000

    jest reducer

---
+++ frontend/jest/feed.test.js
@@ -0,0 +1,202 @@
+import feed from '../src/reducers/feed';
+import * as types from '../src/actions';
+
+describe('feed reducer', ()=> {
+  it('should return the initial state', ()=> {
+    expect(
+      feed(undefined, {})
+    ).toEqual({
+        desiredFeedCount: 0,
+        feedList: {},
+        orderedFeedIdList: []
+      })
+  })
+
+  it('should handle SET_FEED_LIST', () => {
+    expect (
+      feed(undefined, {
+        type : types.SET_FEED_LIST,
+        list : [3]
+      })
+    ).toEqual({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: 0,
+        dislike: 0,
+        scope: null,
+        didLike: null,
+        didDislike: null,
+        replyList: {},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : [3]
+    })
+  })
+
+  it('should handle SET_FEED', () => {
+    expect (
+      feed(undefined, {
+        type : types.SET_FEED,
+        feed : {
+          author : 'asdf',
+          contents : 'asdf',
+          scope : 'public'
+        },
+        id : 3
+      })
+    ).toEqual({
+      desiredFeedCount : 0,
+      feedList : {"3" : {
+        author : 'asdf',
+        contents : 'asdf',
+        like : 0,
+        dislike : 0,
+        scope : 'public',
+        didLike : null,
+        didDislike : null,
+        replyList: {},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : []
+    })
+  })
+
+  it('should handle SET_LIKES', () => {
+    expect (
+      feed(undefined, {
+        type : types.SET_LIKES,
+        id : 3,
+        didLike : false,
+        likes : [3]
+      })
+    ).toEqual({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: 1,
+        dislike: null,
+        scope: null,
+        didLike: false,
+        didDislike: null,
+        replyList: {},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : []
+    })
+  })
+
+  it('should handle SET_DISLIKES', () => {
+    expect (
+      feed(undefined, {
+        type : types.SET_DISLIKES,
+        id : 3,
+        didDislike : false,
+        dislikes : [3]
+      })
+    ).toEqual({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: null,
+        dislike: 1,
+        scope: null,
+        didLike: null,
+        didDislike: false,
+        replyList: {},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : []
+    })
+  })
+
+  it('should handle SET_REPLY_LIST', () => {
+    expect (
+      feed({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: 0,
+        dislike: 0,
+        scope: null,
+        didLike: null,
+        didDislike: null,
+        replyList: {},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : []
+      }, {
+        type : types.SET_REPLY_LIST,
+        list : [3],
+        feedId : 3
+      })
+    ).toEqual({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: 0,
+        dislike: 0,
+        scope: null,
+        didLike: null,
+        didDislike: null,
+        replyList: {"3" : {
+          author: null,
+          contents: null
+        }},
+        orderedReplyIdList : [3]
+      }},
+      orderedFeedIdList : []
+    })
+  })
+
+  it('should handle SET_REPLY', () => {
+    expect (
+      feed({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: 0,
+        dislike: 0,
+        scope: null,
+        didLike: null,
+        didDislike: null,
+        replyList: {},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : []
+      }, {
+        type : types.SET_REPLY,
+        feedId : 3,
+        id : 3,
+        replyId : 3,
+        reply : {
+          author : 'asdf',
+          contents : 'asdf'
+        },
+      })
+    ).toEqual({
+      desiredFeedCount : 0,
+      feedList : {"3" : { 
+        author: null,
+        contents: null,
+        like: 0,
+        dislike: 0,
+        scope: null,
+        didLike: null,
+        didDislike: null,
+        replyList: {"3" :{
+          author: 'asdf',
+          contents: 'asdf'
+        }},
+        orderedReplyIdList : []
+      }},
+      orderedFeedIdList : []
+    })
+  })
+})
---
+++ frontend/jest/server.test.js
@@ -0,0 +1,180 @@
+import server, {serverInitialState} from '../src/reducers/server';
+import * as types from '../src/actions';
+
+describe('server reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      server(undefined, {})
+    ).toEqual({
+      ID: '',
+      PW: '',
+      hash: null,
+      newID: '',
+      newPW: '',
+      retypePW: '',
+      isLogin: true,
+      loggedIn: false,
+      errorMsg: null,
+      onTimeline: false,
+      timelineUser: null
+    })
+  })
+
+  it('should handle SETID', () => { 
+    expect (
+      server({}, {
+        type : types.SETID,
+        ID : 'asdf'
+      })
+    ).toEqual({
+      ID : 'asdf'
+    })
+  })
+
+  it('should handle SETPW', () => { 
+    expect (
+      server({}, {
+        type : types.SETPW,
+        PW : 'asdf'
+      })
+    ).toEqual({
+      PW : 'asdf'
+    })
+  })
+
+  it('should handle NEWID', () => { 
+    expect (
+      server({}, {
+        type : types.NEWID,
+        newID : 'asdf'
+      })
+    ).toEqual({
+      newID : 'asdf'
+    })
+  })
+
+  it('should handle NEWPW', () => { 
+    expect (
+      server({}, {
+        type : types.NEWPW,
+        newPW : 'asdf'
+      })
+    ).toEqual({
+      newPW : 'asdf'
+    })
+  })
+
+  it('should handle RETYPEPW', () => { 
+    expect (
+      server({}, {
+        type : types.RETYPEPW,
+        retypePW : 'asdf'
+      })
+    ).toEqual({
+      retypePW : 'asdf'
+    })
+  })
+
+  it('should handle TOFEED', () => { 
+    expect (
+      server({}, {
+        type : types.TOFEED
+      })
+    ).toEqual({
+      onTimeline: false
+    })
+  })
+
+  it('should handle TOMAIN', () => { 
+    expect (
+      server({}, {
+        type : types.TOMAIN,
+      })
+    ).toEqual({
+      isLogin : true,
+      ID : '',
+      PW : '',
+      onTimeline : false
+    })
+  })
+
+  it('should handle TOSIGNUP', () => { 
+    expect (
+      server({}, {
+        type : types.TOSIGNUP
+      })
+    ).toEqual({
+      isLogin : false,
+      newID : '',
+      newPW : '',
+      retypePW : '',
+      onTimeline: false
+    })
+  })
+
+  it('should handle TOTIMELINE', () => { 
+    expect (
+      server({}, {
+        type : types.TOTIMELINE,
+        username : 'asdf'
+      })
+    ).toEqual({
+      onTimeline: true,
+      timelineUser : 'asdf'
+    })
+  })
+
+  it('should handle LOGIN_SUCCESS', () => { 
+    expect (
+      server({}, {
+        type : types.LOGIN_SUCCESS,
+        hash : 'asdf',
+      })
+    ).toEqual({
+      loggedIn : true,
+      hash : 'asdf',
+      onTimeline : false
+    })
+  })
+
+  it('should handle LOGIN_PAGE_ERROR', () => { 
+    expect (
+      server({}, {
+        type : types.LOGIN_PAGE_ERROR,
+        msg : 'asdf'
+      })
+    ).toEqual({
+      ID: '',
+      PW: '',
+      hash: null,
+      newID: '',
+      newPW: '',
+      retypePW: '',
+      isLogin: true,
+      loggedIn: false,
+      errorMsg: 'asdf',
+      onTimeline: false,
+      timelineUser: null
+    })
+  })
+
+  it('should handle LOGOUT', () => { 
+    expect (
+      server({}, {
+        type : types.LOGOUT
+      })
+    ).toEqual({
+      ID: '',
+      PW: '',
+      hash: null,
+      newID: '',
+      newPW: '',
+      retypePW: '',
+      isLogin: true,
+      loggedIn: false,
+      errorMsg: null,
+      onTimeline: false,
+      timelineUser: null
+    })
+  })
+})
