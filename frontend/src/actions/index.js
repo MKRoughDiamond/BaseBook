1da02032e0fe31d1bcc82d76baed9094d76ee095
greeksharifa <greeksharifa@gmail.com>
2017-05-29 22:39:39 +0900

    adsfdasfas

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -38,6 +38,7 @@ export const GET_CHAT = 'GET_CHAT';
 export const SET_CHAT = 'SET_CHAT';
 export const POST_CHAT = 'POST_CHAT';
 export const TOFEED = 'TOFEED';
+
 export const TOTIMELINE = 'TOTIMELINE';
 export const GET_TIMELINE_LIST = 'GET_TIMELINE_LIST';
 
@@ -51,6 +52,10 @@ export const DELETE_REPLY = 'DELETE_REPLY';
 
 export const START_SOUND = 'START_SOUND';
 export const END_SOUND = 'END_SOUND';
+
+export const TOHASHFEED = 'TOHASHFEED';
+export const GET_HASHFEED_LIST = 'GET_HASHFEED_LIST';
+
 
 export function setID(value) {
   return {
@@ -369,3 +374,18 @@ export function endSound() {
     type: END_SOUND
   };
 }
+
+export function toHashFeed(tagname) {
+  return {
+    type : TOHASHFEED,
+    tagname : tagname
+  };
+}
+
+export function getHashFeedList() {
+  return {
+    type: GET_HASHFEED_LIST
+  };
+}
+
+
