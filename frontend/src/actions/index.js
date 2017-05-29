23ac8ca20392b03a02b1d151cc3dfaa1c8ac5165
greeksharifa <greeksharifa@gmail.com>
2017-05-29 22:04:53 +0900

    hashtag frontend

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -8,128 +8,155 @@ export const TOSIGNUP = 'TOSIGNUP';
 export const LOGIN ='LOGIN';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_PAGE_ERROR = 'LOGIN_PAGE_ERROR';
+
 export const GET_FEED_LIST = 'GET_FEED_LIST';
 export const SET_FEED_LIST = 'SET_FEED_LIST';
 export const GET_FEED = 'GET_FEED';
 export const SET_FEED = 'SET_FEED';
 export const POST_FEED = 'POST_FEED';
+
 export const GET_REPLY_LIST = 'GET_REPLY_LIST';
 export const SET_REPLY_LIST = 'SET_REPLY_LIST';
 export const GET_REPLY = 'GET_REPLY';
 export const SET_REPLY = 'SET_REPLY';
 export const POST_REPLY = 'POST_REPLY';
+
 export const POST_LIKES = 'POST_LIKES';
 export const POST_DISLIKES = 'POST_DISLIKES';
 export const GET_LIKES = 'GET_LIKES';
 export const GET_DISLIKES = 'GET_DISLIKES';
 export const SET_LIKES = 'SET_LIKES';
 export const SET_DISLIKES = 'SET_DISLIKES';
+
 export const TOCHAT = 'TOCHAT';
 export const START_CHAT = 'START_CHAT';
 export const GET_CHAT_ROOM_ID = 'GET_CHAT_ROOM_ID';
+
 export const GET_CHAT_LIST = 'GET_CHAT_LIST';
 export const SET_CHAT_LIST = 'SET_CHAT_LIST';
 export const GET_CHAT = 'GET_CHAT';
 export const SET_CHAT = 'SET_CHAT';
 export const POST_CHAT = 'POST_CHAT';
 export const TOFEED = 'TOFEED';
+
 export const TOTIMELINE = 'TOTIMELINE';
 export const GET_TIMELINE_LIST = 'GET_TIMELINE_LIST';
+
 export const GET_USER_LIST = 'GET_USER_LIST';
 export const SET_USER_LIST = 'SET_USER_LIST';
 export const USER_QUERY = 'USER_QUERY';
 export const LOGOUT = 'LOGOUT';
+
 export const DELETE_FEED = 'DELETE_FEED';
 export const DELETE_REPLY = 'DELETE_REPLY';
+
 export const START_SOUND = 'START_SOUND';
 export const END_SOUND = 'END_SOUND';
+
 export const TOHASHFEED = 'TOHASHFEED';
 export const GET_HASHFEED_LIST = 'GET_HASHFEED_LIST';
+
+
 export function setID(value) {
   return {
     type : SETID,
     ID : value
   };
 }
+
 export function setPW(value) {
   return {
     type : SETPW,
     PW : value
   };
 }
+
 export function newID(value) {
   return {
     type : NEWID,
     newID : value
   };
 }
+
 export function newPW(value) {
   return {
     type : NEWPW,
     newPW : value
   };
 }
+
 export function retypePW(value) {
   return {
     type : RETYPEPW,
     retypePW : value
   };
 }
+
 export function toMain() {
   return {
     type : TOMAIN
   };
 }
+
 export function toSignUp() {
   return {
     type : TOSIGNUP
   };
 }
+
 export function toFeed() {
   return {
     type : TOFEED
   };
 }
+
 export function toTimeline(username) {
   return {
     type : TOTIMELINE,
     username : username
   };
 }
+
 export function login() {
   return {
     type: LOGIN
   };
 }
+
 export function loginSuccess(hash) {
   return {
     type: LOGIN_SUCCESS,
     hash: hash
   };
 }
+
 export function loginPageError(msg) {
   return {
     type: LOGIN_PAGE_ERROR,
     msg: msg
   };
 }
+
 export function getFeedList() {
   return {
     type: GET_FEED_LIST
   };
 }
+
 export function setFeedList(list) {
   return {
     type: SET_FEED_LIST,
     list: list
   };
 }
+
 export function getFeed(id) {
   return {
     type: GET_FEED,
     id: id
   };
 }
+
 export function setFeed(id, feed) {
   return {
     type: SET_FEED,
@@ -137,6 +164,7 @@ export function setFeed(id, feed) {
     feed: feed
   };
 }
+
 export function postFeed(contents, scope) {
   return {
     type: POST_FEED,
@@ -144,30 +172,35 @@ export function postFeed(contents, scope) {
     scope: scope
   };
 }
+
 export function postLikes(id) {
   return {
     type: POST_LIKES,
     id: id
   };
 }
+
 export function postDislikes(id) {
   return {
     type: POST_DISLIKES,
     id: id
   };
 }
+
 export function getLikes(id) {
   return {
     type: GET_LIKES,
     id: id
   };
 }
+
 export function getDislikes(id) {
   return {
     type: GET_DISLIKES,
     id: id
   };
 }
+
 export function setLikes(id, likes, didLike) {
   return {
     type: SET_LIKES,
@@ -176,6 +209,7 @@ export function setLikes(id, likes, didLike) {
     didLike: didLike
   };
 }
+
 export function setDislikes(id, dislikes, didDislike) {
   return {
     type: SET_DISLIKES,
@@ -184,12 +218,14 @@ export function setDislikes(id, dislikes, didDislike) {
     didDislike: didDislike
   };
 }
+
 export function getReplyList(feedId) {
   return {
     type: GET_REPLY_LIST,
     feedId: feedId
   };
 }
+
 export function setReplyList(feedId, list) {
   return {
     type: SET_REPLY_LIST,
@@ -197,6 +233,7 @@ export function setReplyList(feedId, list) {
     list: list
   };
 }
+
 export function getReply(feedId, replyId) {
   return {
     type: GET_REPLY,
@@ -204,6 +241,7 @@ export function getReply(feedId, replyId) {
     replyId: replyId
   };
 }
+
 export function setReply(feedId, replyId, reply) {
   return {
     type: SET_REPLY,
@@ -212,6 +250,7 @@ export function setReply(feedId, replyId, reply) {
     reply: reply
   };
 }
+
 export function postReply(feedId, contents) {
   return {
     type: POST_REPLY,
@@ -219,47 +258,55 @@ export function postReply(feedId, contents) {
     contents: contents,
   };
 }
+
 export function toChat() {
   return {
     type: TOCHAT
   };
 }
+
 export function startChat(username) {
   return {
     type: START_CHAT,
     username: username
   };
 }
+
 export function getChatRoomID(chatRoomID) {
   return {
     type: GET_CHAT_ROOM_ID,
     chatRoomID: chatRoomID
   };
 }
+
 export function getChatList(chatRoomID) {
   return {
     type: GET_CHAT_LIST,
     chatRoomID: chatRoomID
   };
 }
+
 export function setChatList(list) {
   return {
     type: SET_CHAT_LIST,
     list: list
   };
 }
+
 export function getChat(chatRoomID) {
   return {
     type: GET_CHAT,
     chatRoomID: chatRoomID
   };
 }
+
 export function setChat(list) {
   return {
     type: SET_CHAT,
     list: list
   };
 }
+
 export function postChat(chatRoomID, contents) {
   return {
     type: POST_CHAT,
@@ -267,39 +314,46 @@ export function postChat(chatRoomID, contents) {
     contents: contents
   };
 }
+
 export function getTimelineList() {
   return {
     type: GET_TIMELINE_LIST
   };
 }
+
 export function getUserList() {
   return {
     type: GET_USER_LIST
   };
 }
+
 export function setUserList(list) {
   return {
     type: SET_USER_LIST,
     list: list
   };
 }
+
 export function userQuery(keyword) {
   return {
     type: USER_QUERY,
     keyword: keyword
   };
 }
+
 export function logout() {
   return {
     type: LOGOUT
   };
 }
+
 export function deleteFeed(id) {
   return {
     type: DELETE_FEED,
     id: id
   };
 }
+
 export function deleteReply(feedId, replyId) {
   return {
     type: DELETE_REPLY,
@@ -307,25 +361,30 @@ export function deleteReply(feedId, replyId) {
     replyId: replyId
   };
 }
+
 export function startSound(url) {
   return {
     type: START_SOUND,
     url: url
   };
 }
+
 export function endSound() {
   return {
     type: END_SOUND
   };
 }
+
 export function toHashFeed(tagname) {
   return {
     type : TOHASHFEED,
     tagname : tagname
   };
 }
+
 export function getHashFeedList() {
   return {
     type: GET_HASHFEED_LIST
   };
 }
+
--- frontend/src/reducers/server.js
+++ frontend/src/reducers/server.js
@@ -1,6 +1,7 @@
 import { SETID, SETPW, NEWID, NEWPW, RETYPEPW,
   TOMAIN, TOSIGNUP, LOGIN_SUCCESS, LOGIN_PAGE_ERROR, TOTIMELINE, TOFEED, LOGOUT, START_SOUND, END_SOUND, TOHASHFEED
 } from '../actions';
+
 const serverInitialState = {
   ID: '',
   PW: '',
@@ -18,49 +19,51 @@ const serverInitialState = {
   onHashFeed: false,
   tagname: '',
 };
+
 const server = (state = serverInitialState, action) => {
   switch(action.type) {
-    case SETID:
-      return Object.assign({}, state, { ID : action.ID });
-    case SETPW:
-      return Object.assign({}, state, { PW : action.PW });
-    case NEWID:
-      return Object.assign({}, state, { newID : action.newID });
-    case NEWPW:
-      return Object.assign({}, state, { newPW : action.newPW });
-    case RETYPEPW:
-      return Object.assign({}, state, { retypePW : action.retypePW });
-    case TOFEED:
-      return Object.assign({}, state, { onTimeline: false });
-    case TOMAIN:
-      return Object.assign({}, state,
-        { isLogin : true,
-          ID : '',
-          PW : '',
-          onTimeline: false});
-    case TOSIGNUP:
-      return Object.assign({}, state,
-        { isLogin : false,
-          newID : '',
-          newPW : '',
-          retypePW : '',
-          onTimeline: false});
-    case TOTIMELINE:
-      return Object.assign({}, state,
-        { onTimeline : true,
-          timelineUser : action.username });
-    case TOHASHFEED:
-      return Object.assign({}, state,
-        { onHashFeed : true,
-          tagname : action.tagname});
-    case LOGIN_SUCCESS:
-      return Object.assign({}, state, { loggedIn : true, hash: action.hash, onTimeline: false});
-    case LOGIN_PAGE_ERROR:
-      return Object.assign({}, serverInitialState, { errorMsg : action.msg });
-    case LOGOUT:
-      return serverInitialState;
-    default:
-      return state;
+  case SETID:
+    return Object.assign({}, state, { ID : action.ID });
+  case SETPW:
+    return Object.assign({}, state, { PW : action.PW });
+  case NEWID:
+    return Object.assign({}, state, { newID : action.newID });
+  case NEWPW:
+    return Object.assign({}, state, { newPW : action.newPW });
+  case RETYPEPW:
+    return Object.assign({}, state, { retypePW : action.retypePW });
+  case TOFEED:
+    return Object.assign({}, state, { onTimeline: false });
+  case TOMAIN:
+    return Object.assign({}, state,
+      { isLogin : true,
+        ID : '',
+        PW : '',
+        onTimeline: false});
+  case TOSIGNUP:
+    return Object.assign({}, state,
+      { isLogin : false,
+        newID : '',
+        newPW : '',
+        retypePW : '',
+        onTimeline: false});
+  case TOTIMELINE:
+    return Object.assign({}, state,
+      { onTimeline : true,
+        timelineUser : action.username });
+  case TOHASHFEED:
+    return Object.assign({}, state,
+      { onHashFeed : true,
+        tagname : action.tagname});
+  case LOGIN_SUCCESS:
+    return Object.assign({}, state, { loggedIn : true, hash: action.hash, onTimeline: false});
+  case LOGIN_PAGE_ERROR:
+    return Object.assign({}, serverInitialState, { errorMsg : action.msg });
+  case LOGOUT:
+    return serverInitialState;
+  default:
+    return state;
   }
 };
+
 export default server;
