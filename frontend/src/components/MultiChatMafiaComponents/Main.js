327ba3aaac6c07b4bef51b1ab5827ea82ba55122
dnsdhrj <dnsdhrj123@gmail.com>
2017-06-16 15:16:22 +0900

    Make autoscrolling

--- frontend/src/components/MultiChatMafiaComponents/Main.js
+++ frontend/src/components/MultiChatMafiaComponents/Main.js
@@ -9,11 +9,23 @@ class MultiChatMain extends React.Component {
     super(props);
     this.handlePostMultiChat = this.handlePostMultiChat.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.beingScrolled = false;
+    this.lastTick = false;
   }
+  
   componentDidMount() {
     this.props.getMultiChatList(this.props.multichatRoomID);
   }
 
+  componentDidUpdate() {
+    if(!this.beingScrolled) {
+      this.lastTick = true;
+      document.getElementById('chatting-content').scrollTop = 1000000;
+    }
+  }
+  
   handlePostMultiChat() {
     const contents = document.getElementById('new-chat-text').value;
     document.getElementById('new-chat-text').value = '';
@@ -26,19 +38,30 @@ class MultiChatMain extends React.Component {
       this.handlePostMultiChat();
   }
 
+  handleScroll() {
+    if(!this.lastTick)
+      this.beingScrolled = true;
+    this.lastTick = false;
+  }
+
+  handleMouseUp() {
+    this.beingScrolled = false;
+    this.lastTick = true;
+  }
+
   render() {
     const multichatList = this.props.multichatList;
     return (
       <div id="main-wrapper">
         <TopBar/>
-        <div id="main-content">
+        <div id="main-content" onClick={this.handleMouseUp}>
           <div id="multichat-title">
             <div id="multichat-room-user-list">
               { 'Chatting Room '+this.props.multichatRoomID }
             </div>
           </div>
           <div id="chatting-main">
-            <div id="chatting-content">
+            <div id="chatting-content" onScroll={this.handleScroll} onMouseUp={this.handleMouseUp}>
               {multichatList.map( (item) => {
                 return <Entry index={item.id}/>;
               })}
